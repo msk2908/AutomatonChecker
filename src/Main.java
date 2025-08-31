ae5f9ff0d530c0b5aa333c2ae92bde949b9cee96
msk2908 <s8mrklee@stud.uni-saarland.de>
2025-08-31 20:25:32 +0200

    something with input

--- src/Main.java
+++ src/Main.java
@@ -14,7 +14,36 @@ import java.util.stream.Stream;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Input a = new Input("a");
+        Input b = new Input("b");
+        HashMap aTransitions = new HashMap();
+        List<String> transA = new ArrayList<>();
+        transA.add("A");
+        transA.add("A");
+        transA.add("B");
+        aTransitions.put(a, transA);
+        transA.remove("A");
+        transA.remove("A");
+        aTransitions.put(b, transA);
 
+        HashMap bTransitions = new HashMap();
+        List<String> transB = new ArrayList<>();
+        transB.add("A");
+        transB.add("A");
+        transB.add("B");
+        bTransitions.put(a, transB);
+        transB.remove("A");
+        transB.remove("A");
+        bTransitions.put(b, transB);
+
+        State A = new State("A", aTransitions, false, true);
+        State B = new State("B", bTransitions, true, false);
+        List<State> s1 = new ArrayList<>();
+        s1.add(A);
+        s1.add(B);
+        Nea default1 = new Nea(s1);
+
+        boolean starting = false;
         String flag = "y";
         List<State> states = new ArrayList<>();
 
@@ -32,8 +61,11 @@ public class Main {
             System.out.println("Is the state " + state + " terminal? y/n ");
             boolean terminal = br.readLine().equals("y");
 
-            System.out.println("Is the state " + state + " a starting state? y/n ");
-            boolean starting = br.readLine().equals("y");
+            if (!starting) {
+                //get only one starting state
+                System.out.println("Is the state " + state + " a starting state? y/n ");
+                starting = br.readLine().equals("y");
+            }
 
             State currentState = new State(state, new HashMap<>(), terminal, starting);
             states.add(currentState);
@@ -47,13 +79,25 @@ public class Main {
 
         for (State state : states) {
             // get transitions for every state, put into Hashmap
-            System.out.println("How many transitions does " + state.name + " have? ");
             // Creates a reader instance which takes
             // input from standard input - keyboard
+
             Scanner reader = new Scanner(System.in);
 
             // nextInt() reads the next integer from the keyboard
-            int number = reader.nextInt();
+            int number = -1;
+            while (number < 0) {
+                number = -1;
+                System.out.println("How many transitions does " + state.name + " have? ");
+                try {
+                    String num = reader.nextLine();
+                    number = Integer.parseInt(num);
+                    break;
+                } catch (Exception e) {
+                    System.out.println("not a valid number!");
+                }
+            }
+
 
             for (int j = 0; j < number; j++) {
                 System.out.println("Please give one possible transition for state " + state.name);
@@ -71,8 +115,7 @@ public class Main {
             }
 
         }
-        System.out.println("lol");
-
+        System.out.println("debugprint");
     }
 
     public static boolean checkState(String name, List<State> states) {
--- src/Nea.java
+++ src/Nea.java
@@ -3,16 +3,8 @@ import java.util.List;
 
 public class Nea {
     List<State> states;
-    HashMap<Input, List<State>> transitions;
-    State startingState;
-    List<State> finalStates;
-    Alphabet inputAlphabet;
 
-    public Nea(List<State> states, HashMap<Input, List<State>> transitions, State startingState, List<State> finalStates, Alphabet inputAlphabet) {
-        this.finalStates = finalStates;
-        this.inputAlphabet = inputAlphabet;
-        this.startingState = startingState;
+    public Nea(List<State> states) {
         this.states = states;
-        this.transitions = transitions;
     }
 }
